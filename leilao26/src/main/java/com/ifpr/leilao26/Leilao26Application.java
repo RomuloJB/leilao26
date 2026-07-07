51156be23e6860d882e5f95cebe8a60b26911ecb
RomuloJB <jordaobogoni@gmail.com>
2026-07-07 20:04:23 -0300

    first commit

---
+++ leilao26/src/main/java/com/ifpr/leilao26/Leilao26Application.java
@@ -0,0 +1,13 @@
+package com.ifpr.leilao26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leilao26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leilao26Application.class, args);
+	}
+
+}
---
+++ leilao26/src/test/java/com/ifpr/leilao26/Leilao26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpr.leilao26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leilao26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
