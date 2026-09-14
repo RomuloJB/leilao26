6f9e2a523a2027d6737dde51f0579f396ed2fbf6
RomuloJB <jordaobogoni@gmail.com>
2026-09-14 11:40:33 -0300

    fix: corrigindo anotacoes e importacoes

--- leilao26/src/main/java/com/ifpr/leilao26/security/JwtAuthenticationFilter.java
+++ leilao26/src/main/java/com/ifpr/leilao26/security/JwtAuthenticationFilter.java
@@ -3,7 +3,6 @@ package com.ifpr.leilao26.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,6 +17,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -29,9 +29,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private CustomUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                     @NonNull HttpServletResponse response,
-                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                     @NotNull HttpServletResponse response,
+                                     @NotNull FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
 
